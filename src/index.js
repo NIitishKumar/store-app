f7f0a82c9782aeb9773b1e074f48f12e9e6d0325
nitish.kumar <nitish.kumar@team.cliffex.com>
2022-10-29 16:15:14 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Product from "./Product/Product";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// redux
+import { Provider } from "react-redux";
+import { store } from "./Store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Product />
+    </Provider>
   </React.StrictMode>
 );
 
